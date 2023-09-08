1897b28a67d2dad165fc3f32e1b220ff5dc3f61e
nikita stepanov <nist1701@gmail.com>
2023-09-08 19:46:27 +0300

    updated routes

--- src/components/protected-routes/ProtectedRoutes.jsx
+++ src/components/protected-routes/ProtectedRoutes.jsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoutes = ({ loggedIn }) => {
-  return loggedIn ? <Outlet /> : <Navigate to='/' />;
+  const [redirectTo, setRedirectTo] = useState(null);
+
+  useEffect(() => {
+    if (loggedIn === null) return;
+    setRedirectTo('/');
+  }, [loggedIn]);
+
+  return loggedIn ? <Outlet /> : <Navigate to={redirectTo} />;
 };
 
 export default ProtectedRoutes;
